dfd34bdcf985257f10984d732d19785f8d70b016
ammarc <ammar.ahmed@live.com>
2019-01-07 19:02:22 +1100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ]
+      },
+    ]
+  },
+  plugins: [htmlPlugin]
+};
